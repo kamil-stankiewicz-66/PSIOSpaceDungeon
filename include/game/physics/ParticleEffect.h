7c557a69fbcfeff5e1c3cbf56d655b593d24afea
Kamil Stankiewicz <kamil.stankiewicz@student.put.poznan.pl>
2025-06-07 09:51:40 +0200

    feat(particle): add ParticleEffect class

---
+++ include/game/physics/ParticleEffect.h
@@ -0,0 +1,68 @@
+#ifndef PARTICLEEFFECT_H
+#define PARTICLEEFFECT_H
+
+#include "engine/component/Renderable.h"
+#include "engine/object/Object.h"
+#include <random>
+
+class Particle : public VSprite
+{
+    friend class ParticleEffect;
+
+    //to set
+    Vector2 dir;
+    float speed;
+    float lifeTime;
+
+    //calculation
+    Vector2 pos;
+    float age;
+};
+
+class ParticleEffect : public GameObject
+{
+    vector<unique_ptr<Particle>> particles;
+
+    shared_ptr<sf::Texture> texture;
+    sf::Color color;
+    Vector2 scale;
+
+    float spread;
+
+    float speed;
+    float speed_diff;
+
+    float lifeTime;
+    float lifeTime_diff;
+
+    uint particleNum;
+    uint particleNum_diff;
+
+    //random engine
+    mt19937 rng;
+
+    //helpers
+    Vector2 getRndDir(const Vector2& dir);
+    float getRndFloat(const float& val, const float& diff);
+    uint getRndUInt(const uint& val, const uint& diff);
+
+    //engine
+    virtual void onUpdate(float dt) override;
+
+public:
+    void invoke(const Vector2& dir);
+
+    //setters
+    void setTexture(const string_view textureRef);
+    void setColor(const sf::Color& color);
+    void setScale(const Vector2& scale);
+    void setSpread(const float& spread);
+    void setSpeed(const float& speed);
+    void setSpeedDiff(const float& speed_diff);
+    void setLifeTime(const float& lifeTime);
+    void setLifeTimeDiff(const float& lifeTime_diff);
+    void setParticleNum(const uint& particleNum);
+    void setParticleNumDiff(const uint& particleNum_diff);
+};
+
+#endif // PARTICLEEFFECT_H
---
+++ source/game/physics/ParticleEffect.cpp
@@ -0,0 +1,165 @@
+#include "game/physics/ParticleEffect.h"
+
+
+
+
+///
+/// converters
+///
+
+static float vectorToAngle(const Vector2& v) {
+    return std::atan2(v.y, v.x) * 180.0f / M_PI;
+}
+
+static Vector2 angleToVector(float angle_deg) {
+    float angle_rad = angle_deg * M_PI / 180.0f;
+    return Vector2(std::cos(angle_rad), std::sin(angle_rad));
+}
+
+
+
+
+///
+/// helpers
+///
+
+Vector2 ParticleEffect::getRndDir(const Vector2& dir)
+{
+    float baseAngle = vectorToAngle(dir);
+    uniform_real_distribution<float> dist(-spread * 0.5f, spread * 0.5f);
+    float angleOffset = dist(rng);
+    float finalAngle = baseAngle + angleOffset;
+    return angleToVector(finalAngle);
+}
+
+float ParticleEffect::getRndFloat(const float& val, const float& diff)
+{
+    if (diff > 0.0f)
+    {
+        uniform_real_distribution<float> dist(val - diff * 0.5f, val + diff * 0.5f);
+        return dist(rng);
+    }
+    else
+    {
+        return val;
+    }
+}
+
+uint ParticleEffect::getRndUInt(const uint& val, const uint& diff)
+{
+    if (diff > 0u)
+    {
+        uniform_int_distribution<uint> dist(val - (diff * 0.5f), val + (diff * 0.5f));
+        return dist(rng);
+    }
+    else
+    {
+        return val;
+    }
+}
+
+
+
+
+///
+/// engine
+///
+
+
+void ParticleEffect::onUpdate(float dt)
+{
+    //update
+    for (auto& particle : particles)
+    {
+        particle->pos += particle->dir * 0.075f * dt * particle->speed;
+        particle->age += dt;
+
+        //render
+        particle->render(getGame(), particle->pos, scale, vectorToAngle(particle->dir), false, false);
+    }
+
+
+    //delete old particles
+    particles.erase(remove_if(particles.begin(), particles.end(),
+                                   [](const unique_ptr<Particle>& p){ return p->age >= p->lifeTime; }), particles.end());
+
+}
+
+
+
+
+///
+/// invoke
+///
+
+
+void ParticleEffect::invoke(const Vector2& dir)
+{
+    uint N = getRndUInt(this->particleNum, this->particleNum_diff);
+    particles.reserve(particles.size() + N);
+
+    for (uint n = 0u; n < N; ++n)
+    {
+        auto particle = make_unique<Particle>();
+
+        particle->dir = getRndDir(dir);
+        particle->speed = getRndFloat(this->speed, this->speed_diff);
+        particle->lifeTime = getRndFloat(this->lifeTime, this->lifeTime_diff);
+        particle->pos = getTransformPtr()->get_localPosition();
+
+        particles.emplace_back(std::move(particle));
+    }
+}
+
+
+
+
+///
+/// setter
+///
+
+
+void ParticleEffect::setTexture(const string_view tex) {
+    texture = make_shared<sf::Texture>();
+
+    if (!texture->loadFromFile(tex.data())) {
+        VDebuger::print("<ERROR> :: PARTICLE_EFFECT :: SET_TEXTURE:: cant load texture");
+        texture = nullptr;
+    }
+}
+
+void ParticleEffect::setColor(const sf::Color& color) {
+    this->color = color;
+}
+
+void ParticleEffect::setScale(const Vector2& scale) {
+    this->scale = scale;
+}
+
+void ParticleEffect::setSpread(const float& s) {
+    this->spread = s;
+}
+
+void ParticleEffect::setSpeed(const float& s) {
+    this->speed = s;
+}
+
+void ParticleEffect::setSpeedDiff(const float& sd) {
+    this->speed_diff = sd;
+}
+
+void ParticleEffect::setLifeTime(const float& l) {
+    this->lifeTime = l;
+}
+
+void ParticleEffect::setLifeTimeDiff(const float& ld) {
+    this->lifeTime_diff = ld;
+}
+
+void ParticleEffect::setParticleNum(const uint& pn) {
+    this->particleNum = pn;
+}
+
+void ParticleEffect::setParticleNumDiff(const uint& pnd) {
+    this->particleNum_diff = pnd;
+}
