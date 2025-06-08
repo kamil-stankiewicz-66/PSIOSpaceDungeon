864cd059d2668cd0e97f9166f61df8e381a7d2b4
Kamil Stankiewicz <kamil.stankiewicz@student.put.poznan.pl>
2025-06-08 11:01:41 +0200

    feat(datablock): add serialized structures

--- include/game/core/DataBlock.h
+++ include/game/core/DataBlock.h
@@ -3,16 +3,45 @@
 
 using uint = unsigned int;
 
+
+///
+/// \brief Static level data struct.
+/// \details Stores level data and passes it between scenes
+///
+
 struct LevelData
 {
     static uint level_nr;
 };
+
+
+/// [serializable]
+struct SLevelData
+{
+    uint level_nr;
+};
+
+
+
+///
+/// \brief Static player data class.
+/// \details stores player data and passes it between scenes
+///
 
 struct PlayerData
 {
     static uint exp_points;
     static uint weapon_id;
     static uint coins;
+};
+
+
+/// [serializable]
+struct SPlayerData
+{
+    uint exp_points;
+    uint weapon_id;
+    uint coins;
 };
 
 #endif // DATABLOCKS_H
